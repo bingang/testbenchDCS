1aabbf701a49020fff2b059f9d9de67c3a2687a2
bingang <b03901195@ntu.edu.tw>
2018-02-28 20:45:39 +0800

    Update CreateDockerNode2.py

--- workspace/CreateDockerNode2.py
+++ workspace/CreateDockerNode2.py
@@ -82,6 +82,6 @@ with open("docker-compose2.yml", "w") as myfile:
     myfile.write("\n")
     myfile.write("  external:")
     myfile.write("\n")
-    myfile.write("    name: mybitcoin")
+    myfile.write("    name: BitcoinNetworkName")
     myfile.write("\n")
 print "Create Done "
